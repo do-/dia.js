414f0114820cd9c10f7536a3040898ddbdb67498
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-01 18:39:23 +0300

    Request: get_params

--- HTTP.js
+++ HTTP.js
@@ -5,14 +5,6 @@ exports.listen = (handler) => http.createServer (handler).listen ($_CONF.listen.
     darn (`Dia.js server running at http://${$_CONF.listen.host}:${$_CONF.listen.port}/`);
 })
 
-exports.get_request = (rq) => {
-    var uri = url.parse (rq.url)
-    var params = new URLSearchParams (uri.search);
-    var $_REQUEST = {}
-    for (var k of ['type', 'id', 'action', 'part']) if (params.has (k)) $_REQUEST [k] = params.get (k)
-    return $_REQUEST
-}
-
 exports.out_json = (rp, code, data) => {
     rp.statusCode = code
     rp.setHeader ('Content-Type', 'application/json')
--- Request.js
+++ Request.js
@@ -1,10 +1,19 @@
 const Dia = require ('./Dia.js')
+const url  = require ('url')
 
 module.exports = class Request {
 
     constructor (o) {
         this.uuid = Dia.new_uuid ()
         for (i in o) this [i] = o [i]
+        this.get_params ()
+    }    
+    
+    get_params (rq) {
+        let uri = url.parse (this.rq.url)
+        let params = new URLSearchParams (uri.search);
+        this.q = {}
+        for (var k of ['type', 'id', 'action', 'part']) if (params.has (k)) this.q [k] = params.get (k)
     }
-
+    
 }
