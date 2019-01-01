d6c5a6358959670e14c0c020b4dc39c319fca983
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-01 18:53:19 +0300

    renaming

--- Request.js
+++ Request.js
@@ -9,17 +9,23 @@ module.exports = class Request {
         this.get_params ()
     }    
     
-    get_params (rq) {
-        let uri = url.parse (this.rq.url)
+    get_params () {
+        let rq = this.http_request
+        if (rq) this.get_http_params (rq)
+    }
+
+    get_http_params (rq) {
+        let uri = url.parse (rq.url)
         let params = new URLSearchParams (uri.search);
         this.q = {}
         for (var k of ['type', 'id', 'action', 'part']) if (params.has (k)) this.q [k] = params.get (k)
     }
     
     out_json (code, data) {
-        this.rp.statusCode = code
-        this.rp.setHeader ('Content-Type', 'application/json')
-        this.rp.end (JSON.stringify (data))
+        let rp = this.http_response
+        rp.statusCode = code
+        rp.setHeader ('Content-Type', 'application/json')
+        rp.end (JSON.stringify (data))
     }
     
 }
