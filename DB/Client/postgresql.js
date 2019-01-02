bf5f357cd91621bcf0e87f03dbf02ed82869f2d4
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-02 17:36:49 +0300

    sql_select_all

--- DB/Client/postgresql.js
+++ DB/Client/postgresql.js
@@ -7,5 +7,14 @@ module.exports = class {
     async release () {
         return await this.backend.release ()
     }
+    
+    async select_all (sql, params) {
+        if (!params) params = []
+        let label = sql + ' ' + JSON.stringify (params)
+        console.time (label)
+        let result = await this.backend.query (sql, params)
+        console.timeEnd (label)
+        return result.rows
+    }
 
 }
