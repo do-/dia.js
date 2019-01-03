01a75f5fc3cec33beec044a53202521af0ed708c
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-03 18:46:42 +0300

    explicit join

--- DB/Query.js
+++ DB/Query.js
@@ -17,6 +17,10 @@ module.exports = class {
                     let v = this [k]
                     delete this [k]
                     let src = k.trim ()
+
+                    let join_hint
+                    [src, join_hint] = src.split (/\s+ON\s+/)
+                    if (join_hint) this.join_hint = join_hint
                     
                     if (src.indexOf ('(') >= 0) {
                         let [pre, c, post] = src.split (/[\(\)]/)
@@ -83,16 +87,42 @@ this.src = src
                 for (let col of cols) query.cols.push (col)
                  
             }
+            
+            adjust_join () {
+            
+                let adjust_hint = (hint) => {
+                    if (hint.indexOf ('=') > -1) return hint                    
+                    if (hint.indexOf ('.') < 0) hint = `${query.parts[0].alias}.${hint}`                    
+                    return `${hint}=${this.alias}.id`
+                }
+                
+                if (!this.is_root) {
+                    if (this.join_hint) this.join_condition = adjust_hint (this.join_hint)
+                }
+                                    
+                this.sql = '\n\t'
+                
+                if (!this.is_root) this.sql += 'LEFT JOIN '
+                this.sql += this.table
+                if (this.table != this.alias) this.sql += ` AS ${this.alias}`
+                if (!this.is_root) this.sql += ` ON (${this.join_condition})`
+                        
+            }
+           
 
         }
 
         this.parts = other.map ((x) => new this.Part (x))
         this.parts [0].is_root = true
         for (let part of this.parts) part.adjust_cols ()
+        for (let part of this.parts) part.adjust_join ()
+        
+        let get_sql = (x) => x.sql
         
         this.sql = 'SELECT '
-        this.sql += this.cols.map ((col) => col.sql)
+        this.sql += this.cols.map (get_sql)
         this.sql += '\nFROM '
+        this.sql += this.parts.map (get_sql).join ('')
 
     }
 
