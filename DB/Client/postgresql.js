1227876b0a486fea124f0f191786b61479623cda
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-05 15:21:06 +0300

    upsert

--- DB/Client/postgresql.js
+++ DB/Client/postgresql.js
@@ -60,7 +60,38 @@ module.exports = class extends Dia.DB.Client {
         let q = new Dia.DB.Query (this.model, def)       
         let [limited_sql, limited_params] = this.to_limited_sql_params (q.sql, q.params, 1)
         let getter = q.parts [0].cols.length == 1 ? this.select_scalar : this.select_hash
-        return await getter.call (this, q.sql, q.params)
+        return getter.call (this, q.sql, q.params)
+    }
+    
+    async upsert (table, data, key) {
+    
+        if (Array.isArray (data)) {
+            for (let d in data) await this.upsert (table, d, key)
+            return
+        }
+        
+        if (typeof data !== 'object') throw 'upsert called with wrong argument: ' + JSON.stringify ([table, data])
+        if (data === null) throw 'upsert called with null argument: ' + JSON.stringify ([table, data])
+
+        let def = this.model.tables [table]
+        if (!def) throw 'Table not found: ' + table
+
+        if (!key) key = [def.pk]
+        if (!Array.isArray (key)) key = [key]
+        
+        let [fields, args, set, params] = [[], [], [], []]
+        
+        for (let k in data) {
+            fields.push (k)
+            args.push ('?')
+            params.push (data [k])
+            if (key.indexOf (k) < 0) set.push (`${k}=COALESCE(EXCLUDED.${k},${table}.${k})`)
+        }
+// TODO: eliminate "fake"!!!        
+        let sql = `INSERT INTO ${table} (${fields}) VALUES (${args}) ON CONFLICT (${key}) WHERE fake = 0 DO UPDATE SET ${set} RETURNING id`
+
+        return this.select_scalar (sql, params)
+        
     }
     
 }
