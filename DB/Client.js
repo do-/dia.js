caf5ebaf695e1488f31e51c78d353e00e8b24349
Dmitry Ovsyanko <do_@pochta.ru>
2019-02-01 22:42:33 +0300

    select_vocabulary: no `fake` field

--- DB/Client.js
+++ DB/Client.js
@@ -13,13 +13,10 @@ module.exports = class {
     async select_vocabulary (t, o = {}) {
     
         if (!o.order) o.order = 2
-
-        let filter = 'fake = 0'
-        if (o.filter) filter += ` AND ${o.filter}`
         
         if ((o.label = o.label || 'label') != 'label') o.label = o.label.replace (/ AS.*/, '') + ' AS label'
 
-        return this.select_all (`SELECT ${this.model.tables [t].pk} id, ${o.label} FROM ${t} WHERE ${filter} ORDER BY ${o.order}`)
+        return this.select_all (`SELECT ${this.model.tables [t].pk} id, ${o.label} FROM ${t} WHERE ${o.filter || '1=1'} ORDER BY ${o.order}`)
 
     }
 
