3f2b8215ab43449603c11c30a65f6b487d0ffae6
Dmitry Ovsyanko <do_@pochta.ru>
2019-10-02 20:13:05 +0300

    CachedCookieSession: 2nd (users) cache removed

--- Content/Handler/HTTP/Session/CachedCookieSession.js
+++ Content/Handler/HTTP/Session/CachedCookieSession.js
@@ -7,50 +7,32 @@ module.exports = class extends CookieSession {
 		super (h, o)
 	}
 		
-    async get_user_by_id () {
-		throw 'get_user_by_id is not defined'
-    }
-
     async keep_alive () {
-    	await this.o.sessions.to_set (this.id, this.user.uuid)
-    	return this.user
+    	return await this.o.sessions.to_set (this.id, this.user)
     }
 
-    async start () {  
+    async start () {
         await super.start ()
-        this.keep_alive ()
+        await this.keep_alive ()
     }
     
-    async finish () {            
-        await super.finish ()
-        this.o.sessions.to_del (this.id)
+    async finish () {
+    	Promise.all ([
+    		super.finish (),
+    		this.o.sessions.to_del (this.id)
+    	])
     }
 
-    restrict_access () {
-    	if (!this.h.is_anonymous ()) throw '401 Authenticate first'
-        return undefined
-    }
-        
     async get_user () {
 
         if (!this.id) return this.h.no_user ()
         
-        let uuid = await this.o.sessions.to_get (this.id)
+        if (this.user = await this.o.sessions.to_get (this.id)) return this.keep_alive ()
 
-        if (!uuid) {
-        	darn (`session ${this.id} not found`)
-        	return this.h.no_user ()
-        }
-        
-        this.user = await this.o.users.to_get (uuid, this.get_user_by_id)
+		darn (`session ${this.id} not found`)
+		
+		return this.h.no_user ()
 
-        if (!this.user) {
-        	darn (`session ${this.id}: valid user ${uuid} not found`)
-        	return this.h.no_user ()
-        }
-
-        return this.keep_alive ()
-                        
     }
 
 }
