1a49ec49489e24dc00156c33bc439b67eac54a08
Dmitry Ovsyanko <do_@pochta.ru>
2019-10-28 21:17:50 +0300

    EluStatic optimization

--- Content/Handler/HTTP/EluStatic.js
+++ Content/Handler/HTTP/EluStatic.js
@@ -1,6 +1,7 @@
 const HTTP = require ('../HTTP')
 const fs   = require ('fs')
 const path = require ('path')
+const zlib = require ('zlib')
 
 module.exports = class extends HTTP.Handler {
 
@@ -11,11 +12,16 @@ module.exports = class extends HTTP.Handler {
 		let url = this.http.request.url
 		
 		if (url.charAt (1) == '_' && url.charAt (2) == '_') {
-			url = '/_/' + url.substr (1).split ('/').slice (1).join ('/')
+			url = '/_/' + url.substr (1).split ('/').slice (1).join ('/')			
+		}
+		else if (url == '/favicon.ico' || url == '/robots.txt') {
+			url = '/_mandatory_content' + url 
 		}
 		else {
 			url = '/index.html'
 		}
+		
+		if (url != '/index.html') this.http.response.setHeader ('Expires', 'Sun, 01 Jan 2119 00:00:00 GMT')
 				
 		let abs = path.resolve ('../../front/root' + url)		
 		
@@ -33,19 +39,33 @@ module.exports = class extends HTTP.Handler {
 			return 'application/javascript; charset=utf-8'
 		case 'css':
 			return 'text/css'
+		case 'txt':
+			return 'text/plain'
 		case 'svg':
 			return 'image/svg+xml'
 		case 'png':
 			return 'image/png'
+		case 'ico':
+			return 'image/x-icon'
 		default: 
 			return 'application/octet-stream'
 	}}
 	
     send_out_data (fn) {
+
         let rp = this.http.response
         rp.statusCode = 200
         rp.setHeader ('Content-Type', this.mime (fn.split ('.').pop ()))
-        rp.end (fs.readFileSync (fn))
+
+		let str = fs.createReadStream (fn)
+		
+		if (/gzip/.test (this.http.request.headers ['accept-encoding'])) {
+			rp.setHeader ('Content-Encoding', 'gzip')
+			str = str.pipe (zlib.createGzip ())
+		}
+		
+		return str.pipe (rp)
+
     }
     
     get_log_banner () {
