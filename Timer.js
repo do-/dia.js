2737830905a4ebf7eaa3f1b6b8e3b2cba000134f
Dmitry Ovsyanko <do_@pochta.ru>
2019-12-14 12:08:25 +0300

    Timer logging improved

--- Timer.js
+++ Timer.js
@@ -8,17 +8,23 @@ module.exports = class {
 		
 		this.o = o
 		
+		this.log_label = [o.label, 'timer: '].filter (i => i).join (' ')
+		
+	}
+	
+	log (s) {
+		darn (this.log_label + s)
 	}
 	
 	from_to (from, to) {
-		darn (`Timer: from_to (${from}, ${to}) called`)
+		this.log (`from_to (${from}, ${to}) called`)
 		this.o.from = from
 		this.o.to   = to
-		darn ('Timer: o = ' + JSON.stringify (this.o))
+		this.log ('o = ' + JSON.stringify (this.o))
 	}
 	
 	clear () {
-		darn ('Timer: clear () called')
+		this.log ('clear () called')
 		if (!this.t) return
 		clearTimeout (this.t)
 		delete this.t
@@ -27,25 +33,25 @@ module.exports = class {
 	
 	in (ms) {
 	
-		darn (`Timer: in (${ms}) called`)
+		this.log (`in (${ms}) called`)
 
 		if (ms < 0) ms = 0
 
 		let when = ms + new Date ().getTime ()
 		
-		darn ('Timer: the desired time is ' + new Date (when))
+		this.log ('the desired time is ' + new Date (when))
 
 		if (this.next && when < this.next) {
 		
 			when = this.next
 			
-			darn ('Timer: adjusted to the next period ' + new Date (when))
+			this.log ('adjusted to the next period ' + new Date (when))
 		
 		}
 
 		if (this.o.from) {
 		
-			darn (`Timer: checking for ${this.o.from}..${this.o.to}`)
+			this.log (`checking for ${this.o.from}..${this.o.to}`)
 
 			let dt         = new Date (when)
 
@@ -60,7 +66,7 @@ module.exports = class {
 			
 			if (!is_in) {
 			
-				darn (`Timer: ${dt} is out of ${this.o.from}..${this.o.to}, adjusting`)
+				this.log (`${dt} is out of ${this.o.from}..${this.o.to}, adjusting`)
 				
 				if (is_one_day && !le_to) dt.setDate (1 + dt.getDate ())
 				
@@ -72,17 +78,17 @@ module.exports = class {
 				
 				when = dt.getTime ()
 			
+				this.log ('adjusted to time window ' + new Date (when))
+			
 			}
 						
 		}
-				
-		darn ('Timer: about to schedule at ' + new Date (when))
 
 		if (this.t) {
-		
-			darn ('Timer: was previously scheduled at ' + new Date (this.when))
+					
+			if (this.when <= when) return this.log (`already scheduled at ${new Date (this.when)}, exiting`)
 			
-			if (this.when <= when) return darn ('Timer: nothing to do, exiting')
+			this.log (`cancelling previous schedule at ${new Date (this.when)}`)
 			
 			this.clear ()
 
@@ -90,21 +96,21 @@ module.exports = class {
 		
 		if (this.is_busy) {
 
-			darn ('Timer: busy...')
+			this.log ('busy...')
 
 			let is_reset_to = this.is_reset_to
 			
-			if (is_reset_to >= when) return darn ('Timer: was already reset to ' + new Date (is_reset_to) + ', nothing to do')
+			if (is_reset_to >= when) return this.log ('was already reset to ' + new Date (is_reset_to) + ', nothing to do')
 			
 			this.is_reset_to = when
 
-			return darn ('Timer: reset, now quitting')
+			return this.log ('reset, now quitting')
 
 		}		
 
 		this.t = setTimeout (() => this.run (), when - new Date ().getTime ())
 
-		darn ('Timer: now scheduled at ' + new Date (this.when = when))
+		this.log ('now scheduled at ' + new Date (this.when = when))
 		
 	}
 	
@@ -132,7 +138,7 @@ module.exports = class {
 	
 		this.next = new Date ().getTime () + this.o.period
 	
-		darn ('Timer: run () called')
+		this.log ('run () called, next time may be at ' + new Date (this.next))
 		
 		this.is_busy = 1
 
@@ -149,7 +155,7 @@ module.exports = class {
 		
 		let when = this.is_reset_to; if (when) {
 
-			darn ('Timer: about to reset...')			
+			this.log ('about to reset...')			
 
 			delete this.is_reset_to
 
