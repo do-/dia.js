do-/dia.js

commit c3ea5d83efca4bd564da578bbcc4a53dd4d79a16

add_all_cnt optimization

DB/Client.js
@@ -84,7 +84,10 @@ module.exports = class {
         if (offset == undefined) offset = q.offset
         if (offset == undefined) offset = 0
 
-        let [all, cnt] = await this.select_all_cnt (q.sql, q.params, limit, offset)
+        let [all, cnt] = await Promise.all ([
+        	this.select_all    (q.sql,     q.params, limit, offset),
+        	this.select_scalar (q.sql_cnt, q.params),
+        ])
 
         data [q.parts [0].alias] = all
         data.cnt = cnt

DB/Query.js
@@ -390,6 +390,11 @@ module.exports = class {
             for (let filter of filters) for (let param of filter.params) this.params.push (param)
         }
         
+        if (this.limit) {
+        	this.sql_cnt = 'SELECT COUNT(*) FROM ' + this.parts.filter (p => p.is_root || p.is_inner	).map (get_sql).join ('')
+	        if (filters.length) this.sql_cnt += ' WHERE ' + filters.map (get_sql).join (' AND ')
+        }
+
         if (this.order) this.sql += `\nORDER BY \n\t ${this.order}`
 
     }

commit 3cd415ebac62b85a6894b23c73b97ee815c0d0dc

add_all_cnt FIX (was broken since c3ea5d8)

DB/Client.js
@@ -83,9 +83,11 @@ module.exports = class {
 
         if (offset == undefined) offset = q.offset
         if (offset == undefined) offset = 0
+        
+        let [limited_sql, limited_params] = this.to_limited_sql_params (q.sql, q.params, limit, offset)
 
         let [all, cnt] = await Promise.all ([
-        	this.select_all    (q.sql,     q.params, limit, offset),
+        	this.select_all    (limited_sql, limited_params),
         	this.select_scalar (q.sql_cnt, q.params),
         ])
 