a42e1c33feb0e7678c3a20c2bcb0717c80648ae4
Dmitry Ovsyanko <do_@pochta.ru>
2020-08-26 22:35:14 +0300

    postgresql array insert optimization (COPY TO)

--- DB/Client/postgresql.js
+++ DB/Client/postgresql.js
@@ -179,8 +179,8 @@ module.exports = class extends Dia.DB.Client {
         if (!def) throw 'Table not found: ' + table
 
         if (Array.isArray (data)) {
-            for (let d of data) await this.insert (table, d)
-            return
+        	if (!data.length) return
+			return this.load (Readable.from (data), table, Object.keys (data [0]))
         }
         
         let [fields, args, params] = [[], [], []]
