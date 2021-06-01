do-/dia.js

commit 1eced102b6ec5ea536759b8949fab1e1a83526f0

db logging: before/after

DB/Client/clickhouse.js
@@ -30,24 +30,16 @@ module.exports = class extends Dia.DB.Client {
         params.push (offset)
         return [original_sql + ' LIMIT ? OFFSET ?', params]
     }
-    
-    log_label (sql, params) {
-    
-    	return (this.log_prefix || '') + sql.replace (/\s+/g, ' ').trim ()
-    
-    }
-    
+        
     async select_stream (sql, params, o = {}) {
 
+        let log_event = this.log_start (sql, params)
+
     	sql = this.bind (sql, params)
-
-    	let label = this.log_label (sql); console.time (label)        
-
-        this.backend.log_prefix = this.log_prefix
        	       	
        	let input = await this.backend.responseStream ({}, sql + ' FORMAT JSONEachRow')
 
-		input.on ('end', () => console.timeEnd (label))
+		input.on ('end', () => this.log_finish (log_event))
 
 		let reader = readline.createInterface ({input})
 
@@ -108,12 +100,10 @@ module.exports = class extends Dia.DB.Client {
     async load (is, table, fields) {
     
     	let sql = `INSERT INTO ${table} (${fields})`
-
-    	let label = this.log_label (sql)
-    	
+   
         try {        
         
-        	console.time (label)        
+	        let log_event = this.log_start (sql)
         	
         	let body = new Transform ({transform (chunk, encoding, callback) {
 				
@@ -122,9 +112,7 @@ module.exports = class extends Dia.DB.Client {
 				callback (null, chunk)			
 					
 			}})        	
-			
-	        this.backend.log_prefix = this.log_prefix
-	        
+				        
 			let res_promise = this.backend.response ({}, body)
 			
 			is.pipe (body)
@@ -134,7 +122,7 @@ module.exports = class extends Dia.DB.Client {
         }
         finally {
         
-        	console.timeEnd (label)
+        	this.log_finish (log_event)
         
         }
     
@@ -291,19 +279,15 @@ module.exports = class extends Dia.DB.Client {
     
     async do (sql, params = []) {
     
+        let log_event = this.log_start (sql, params)
+
     	sql = this.bind (sql, params)
-    	
-    	let label = this.log_label (sql)
-        
+    	        
         try {        
-        	console.time (label)        
-	        this.backend.log_prefix = this.log_prefix        	
 			await this.backend.response ({}, sql)			
         }
-        finally {
-        
-        	console.timeEnd (label)
-        
+        finally {        
+            this.log_finish (log_event)        
         }
     
     }        

DB/Pool/clickhouse.js
@@ -22,12 +22,15 @@ module.exports = class extends require ('../Pool.js') {
 
     }
     
-    async acquire () {
+    async acquire (o = {}) {
+    
+    	let {log_meta} = o
+    	
+    	let c = new wrapper (await this.http.acquire ({log_meta}))
 
-    	let c = new wrapper (await this.http.acquire ())
-
+		c.log_meta = log_meta
         c.database = this.database
-    	c.model = this.model
+    	c.model    = this.model
     	
         return c
         

Content/Handler.js
@@ -152,12 +152,17 @@ module.exports = class {
     }
 
     async acquire_resource (pool, k) {
-        let db = await pool.acquire ()
-        db.log_prefix = this.uuid + ' ' + k + ':  '
+
+        let db = await pool.acquire ({log_meta: {request: this, resource_name: k}})
+
         let {product} = pool; if (product) db.product = product
+
         this.__resources.push (db)
+
         if (db.begin && this.is_transactional ()) await db.begin ()
+
         return db
+
     }
 
     async release_resources () {    

DB/Client.js
@@ -337,5 +337,59 @@ module.exports = class {
     async load_schema_table_keys () {}
     
     async load_schema_table_triggers () {}
+    
+    log_format (e) {
+    
+    	let {prefix, uuid, phase, request, resource_name, db_query, duration} = e
+    	
+    	let s = prefix ? prefix + ' ' : ''
+    	
+    	if (request) s += request.uuid + ' '
+    	if (resource_name) s += resource_name + ' '
+
+		switch (phase) {
+			case 'after':
+				return s + '< ' + uuid + ' ' + duration + ' ms'
+			case 'before':
+				let {sql, params} = db_query
+				s += '> ' + uuid + ' ' + sql.replace (/\s+/g, ' ').trim () 
+				if (params && params.length) s += ' ' + JSON.stringify (params)
+				return s
+			default:
+				return null
+		}    	
+    
+    }
+
+    log_write (e) {
+
+    	let s = this.log_format (e); if (s) console.log (s)
+    
+    	return e
+    
+    }
+
+    log_finish (e) {
+    
+		e.phase = 'after'
+		
+		e.duration = (e.ts_to = new Date ()) - e.ts
+    	
+    	this.log_write (e)
+
+    }
+    
+    log_start (sql, params) {
+        	
+    	return this.log_write ({
+			...(this.log_meta || {}),
+			level    : 'info',
+			uuid     : Dia.new_uuid (),
+			db_query : {sql, params},
+			phase    : 'before',
+			ts       : new Date ()
+    	})
+
+    }
 
 }

DB/Client/mssql.js
@@ -38,9 +38,7 @@ module.exports = class extends Dia.DB.Client {
 
     async do (sql, params = []) {
 
-        let label = (this.log_prefix || '') + sql.replace (/\s+/g, ' ') + ' ' + JSON.stringify (params)
-
-        console.time (label)
+        let log_event = this.log_start (sql, params)
 
     	return new Promise ((ok, fail) => {
 
@@ -50,9 +48,9 @@ module.exports = class extends Dia.DB.Client {
 
             params.forEach (p => request.input (p [0], p[1]));
 
-			request.query (sql, function (x) {
+			request.query (sql, x => {
 
-				console.timeEnd (label)
+				this.log_finish (log_event)
 
 				return x ? fail (x) : ok ()
 
@@ -64,9 +62,7 @@ module.exports = class extends Dia.DB.Client {
 
     async select_hash (sql, params) {
 
-        let label = (this.log_prefix || '') + sql.replace (/^\s+/g, '').replace (/\s+/g, ' ') + ' ' + JSON.stringify (params)
-
-        console.time (label)
+        let log_event = this.log_start (sql, params)
 
     	return new Promise ((ok, fail) => {
 
@@ -76,9 +72,9 @@ module.exports = class extends Dia.DB.Client {
 
             params.forEach (p => request.input (p [0], p[1]));
 
-			request.query (sql, function (x, all, fields) {
+			request.query (sql, (x, all, fields) => {
 
-				console.timeEnd (label)
+				this.log_finish (log_event)
 
 				return x ? fail (x) : ok (all.recordset [0] || {})
 
@@ -90,9 +86,7 @@ module.exports = class extends Dia.DB.Client {
 
     async select_all (sql, params) {
 
-        let label = (this.log_prefix || '') + sql.replace (/^\s+/g, '').replace (/\s+/g, ' ') + ' ' + JSON.stringify (params)
-
-        console.time (label)
+        let log_event = this.log_start (sql, params)
 
     	return new Promise ((ok, fail) => {
 
@@ -102,9 +96,9 @@ module.exports = class extends Dia.DB.Client {
 
             params.forEach (p => request.input (p [0], p[1]));
 
-			request.query (sql, function (x, all) {
+			request.query (sql, (x, all) => {
 
-				console.timeEnd (label)
+				this.log_finish (log_event)
 
 				return x ? fail (x) : ok (all.recordset)
 
@@ -116,9 +110,7 @@ module.exports = class extends Dia.DB.Client {
 
     async select_stream (sql, params, o) {
 
-        let label = (this.log_prefix || '') + sql.replace (/^\s+/g, '').replace (/\s+/g, ' ') + ' ' + JSON.stringify (params)
-
-        console.time (label)
+        let log_event = this.log_start (sql, params)
 
         const request = this.backend.request()
 
@@ -158,7 +150,7 @@ module.exports = class extends Dia.DB.Client {
 
     	request.query (sql, params)
 
-    	stream.on ('end', () => console.timeEnd (label))
+    	stream.on ('end', () => this.log_finish (log_event))
 
     	return stream
 

DB/Client/mysql.js
@@ -3,14 +3,6 @@ const Dia = require ('../../Dia.js')
 module.exports = class extends Dia.DB.Client {
 
     async release (success) {
-/*    
-        if (this.backend.is_txn_pending) try {        
-            await this.finish_txn (success)
-        }
-        catch (x) {
-            darn (x)
-        }
-*/
 
         this.backend.release ()
         
@@ -26,15 +18,13 @@ module.exports = class extends Dia.DB.Client {
     
     async do (sql, params = []) {
 
-        let label = (this.log_prefix || '') + sql.replace (/\s+/g, ' ') + ' ' + JSON.stringify (params)
-
-        console.time (label)
+        let log_event = this.log_start (sql, params)
 
     	return new Promise ((ok, fail) => {
     	
-			this.backend.query (sql, params, function (x) {
+			this.backend.query (sql, params, x => {
 			
-				console.timeEnd (label)
+				this.log_finish (log_event)
 						
 				return x ? fail (x) : ok ()
 
@@ -46,15 +36,13 @@ module.exports = class extends Dia.DB.Client {
 
     async select_hash (sql, params) {
     
-        let label = (this.log_prefix || '') + sql.replace (/^\s+/g, '').replace (/\s+/g, ' ') + ' ' + JSON.stringify (params)
-        
-        console.time (label)
+        let log_event = this.log_start (sql, params)
 
     	return new Promise ((ok, fail) => {
     	
-			this.backend.query (sql, params, function (x, all, fields) {
+			this.backend.query (sql, params, (x, all, fields) => {
 			
-				console.timeEnd (label)
+				this.log_finish (log_event)
 						
 				return x ? fail (x) : ok (all [0] || {})
 
@@ -65,16 +53,14 @@ module.exports = class extends Dia.DB.Client {
     }
     
     async select_all (sql, params) {
-    
-        let label = (this.log_prefix || '') + sql.replace (/^\s+/g, '').replace (/\s+/g, ' ') + ' ' + JSON.stringify (params)
         
-        console.time (label)
+        let log_event = this.log_start (sql, params)
 
     	return new Promise ((ok, fail) => {
     	
-			this.backend.query (sql, params, function (x, all, fields) {
+			this.backend.query (sql, params, (x, all, fields) => {
 			
-				console.timeEnd (label)
+				this.log_finish (log_event)
 						
 				return x ? fail (x) : ok (all)
 
@@ -86,13 +72,11 @@ module.exports = class extends Dia.DB.Client {
 
     async select_stream (sql, params, o) {
     	
-        let label = (this.log_prefix || '') + sql.replace (/^\s+/g, '').replace (/\s+/g, ' ') + ' ' + JSON.stringify (params)
-        
-        console.time (label)
+        let log_event = this.log_start (sql, params)
         
     	let stream = this.backend.query (sql, params).stream (o)
     	
-    	stream.on ('end', () => console.timeEnd (label))
+    	stream.on ('end', () => this.log_finish (log_event))
     	
     	return stream
 

DB/Client/postgresql.js
@@ -52,17 +52,15 @@ module.exports = class extends Dia.DB.Client {
     async select_all (original_sql, params = []) {
     
         let sql = this.fix_sql (original_sql)
-        
-        let label = (this.log_prefix || '') + sql.replace (/^\s+/g, '').replace (/\s+/g, ' ') + ' ' + JSON.stringify (params)
-        
-        console.time (label)
+                
+        let log_event = this.log_start (sql, params)
         
         try {
             let result = await this.backend.query (sql, params)
             return result.rows
         }
         finally {
-            console.timeEnd (label)
+            this.log_finish (log_event)
         }
         
     }
@@ -70,16 +68,14 @@ module.exports = class extends Dia.DB.Client {
     async select_stream (original_sql, params, o) {
     	
         let sql = this.fix_sql (original_sql)
-
-        let label = (this.log_prefix || '') + sql.replace (/^\s+/g, '').replace (/\s+/g, ' ') + ' ' + JSON.stringify (params)
         
-        console.time (label)
+        let log_event = this.log_start (sql, params)
         
         let qs = require ('pg-query-stream')
 
     	let stream = this.backend.query (new qs (sql, params, o))
     	
-    	stream.on ('end', () => console.timeEnd (label))
+    	stream.on ('end', () => this.log_finish (log_event))
     	
     	return stream
 
@@ -264,16 +260,14 @@ module.exports = class extends Dia.DB.Client {
 
     	}
 
-        let label = (this.log_prefix || '') + sql.replace (/\s+/g, ' ') + ' ' + JSON.stringify (params)
-
-        console.time (label)
+        let log_event = this.log_start (sql, params)
 
         try {
             return await this.backend.query (sql, params)
         }
         finally {
-            console.timeEnd (label)
-        }
+			this.log_finish (log_event)
+		}
 
     }
 
@@ -361,11 +355,11 @@ module.exports = class extends Dia.DB.Client {
 
 			sql = `COPY ${table} (${cols}) FROM STDIN ${sql}`
 
-	        let label = (this.log_prefix || '') + sql; console.time (label)
+	        let log_event = this.log_start (sql)
 
 			let os = this.backend.query (require ('pg-copy-streams').from (sql))
 
-			os.on ('finish', () => ok (console.timeEnd (label)))
+			os.on ('finish', () => ok (this.log_finish (log_event)))
 
 			os.on ('error', fail)
 			is.on ('error', fail)

DB/Client/sqlite3.js
@@ -46,17 +46,9 @@ module.exports = class extends Dia.DB.Client {
         return [original_sql + ' LIMIT ? OFFSET ?', params]
     }
     
-    log_label (sql, params) {
-    
-    	return (this.log_prefix || '') + sql.replace (/^\s+/g, '').replace (/\s+/g, ' ') + ' ' + JSON.stringify (params)
-    
-    }
-    
     async do (sql, params = []) {
             
-        let label = this.log_label (sql, params)
-        
-        console.time (label)
+        let log_event = this.log_start (sql, params)
 
         return new Promise ((ok, fail) => {
         
@@ -72,29 +64,25 @@ module.exports = class extends Dia.DB.Client {
         	
         	)
         
-        }).finally (() => console.timeEnd (label))
+        }).finally (() => this.log_finish (log_event))
 
     }    
 
     async select_all (sql, params = []) {
             
-        let label = this.log_label (sql, params)
-        
-        console.time (label)
-        
+        let log_event = this.log_start (sql, params)
+                
         return new Promise ((ok, fail) => {
         
         	this.backend.all (sql, params, (x, d) => x ? fail (x) : ok (d))
         
-        }).finally (() => console.timeEnd (label))
+        }).finally (() => this.log_finish (log_event))
 
     }
     
     async select_loop (sql, params, callback, data) {
     
-        let label = this.log_label (sql, params)
-        
-        console.time (label)
+        let log_event = this.log_start (sql, params)
         
         return new Promise ((ok, fail) => {
         
@@ -103,21 +91,19 @@ module.exports = class extends Dia.DB.Client {
         		(x, cnt) => x ? fail (x) : ok (data)
         	)
         
-        }).finally (() => console.timeEnd (label))
+        }).finally (() => this.log_finish (log_event))
     
     }
 
     async select_hash (sql, params) {
     
-        let label = this.log_label (sql, params)
-        
-        console.time (label)
-        
+        let log_event = this.log_start (sql, params)
+                
         return new Promise ((ok, fail) => {
         
         	this.backend.get (sql, params, (x, d) => x ? fail (x) : ok (d || {}))
         
-        }).finally (() => console.timeEnd (label))
+        }).finally (() => this.log_finish (log_event))
 
     }
 

commit d9130e602dc48c8ef87c54f6836e5ca94b67e617

clickhouse load FIX (was broken since 1eced10)

DB/Client/clickhouse.js
@@ -103,9 +103,10 @@ module.exports = class extends Dia.DB.Client {
     
     	let sql = `INSERT INTO ${table} (${fields})`
    
+		let log_event = this.log_start (sql)
+
         try {        
         
-	        let log_event = this.log_start (sql)
         	
         	let body = new Transform ({transform (chunk, encoding, callback) {
 				