838cb79484ddf70ae54c02bafde87e0a2da27501
Dmitry Ovsyanko <do_@pochta.ru>
2021-11-13 15:45:12 +0300

    ConsoleLogger FIX (validation)

--- Log/ConsoleLogger.js
+++ Log/ConsoleLogger.js
@@ -1,3 +1,4 @@
+const assert  = require ('assert')
 const MESSAGE = Symbol.for ('message')
 
 module.exports = class {
@@ -9,30 +10,57 @@ module.exports = class {
 	}
 	
 	after_category (e) {
-
-		const {level, path, resource_name, message} = e
 		
-		let s = level + ' '
+		try {
 
-		{
+			const {uuid, category, parent, level, path, resource_name, message} = e
+			
+			const carp = s => {		
+				try {				
+					return s + ': ' + JSON.stringify (e)
+				}
+				catch (x) {
+					try {				
+						return s + ': ' + JSON.stringify ({uuid, category, parent, level, path, resource_name, message})
+					}
+					catch (x) {
+						return s
+					}
+				}			
+			}
 
-			const {length} = path; for (let i = 0; i < length; i ++) {
+			assert (level    != null, carp ('Level not set'))
+			assert (category != null, carp ('Category not set'))
+			assert (path     != null, carp ('Path not set'))
 
-				if (i > 0) s += '/'
+			let s = level + ' '
 
-				s += path [i]
+			{
+
+				const {length} = path; for (let i = 0; i < length; i ++) {
+
+					if (i > 0) s += '/'
+
+					s += path [i]
+
+				}
 
 			}
 
+			if (resource_name) s += ' ' + resource_name
+
+			if ('get_sigil' in e) s += ' ' + e.get_sigil ()
+
+			s += ' ' + message
+
+			return s
+
 		}
-
-		if (resource_name) s += ' ' + resource_name
+		catch (x) {
 		
-		if ('get_sigil' in e) s += ' ' + e.get_sigil ()
-
-		s += ' ' + message
+			console.error (x)
 		
-		return s
+		}
 
 	}
 	
