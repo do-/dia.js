c04d1c4facb8db436e310458d28722a3bfe2bcc2
Dmitry Ovsyanko <do_@pochta.ru>
2021-11-13 15:46:47 +0300

    WrappedError FIXED (was broken during timer refactoring), cleaned up

--- Log/WrappedError.js
+++ Log/WrappedError.js
@@ -1,27 +1,17 @@
 module.exports = class extends Error {
 
     constructor (e, o = {}) {
-
-		super (e.message)
-
-		this.stack = e.stack
-		this.level = 'error'
+    
+    	super (e.message)
 		
-		const {log_meta} = o, {parent} = log_meta
+		this.stack = e.stack
+		
+		for (let [k, v] of [...Object.entries ({...e, ...(o || {})})])
 
-		this.path     = parent.path		
-		this.category = parent.category
-
-		for (let [k, v] of [...Object.entries ({...e, ...(o.log_meta || {})})])
-
-			if (!(k in this) && v !== undefined) 
+			if (!(k in this) && v !== undefined)
 				
 				this [k] = v
 
-	}
-
-	get_sigil () {
-		return '!'
 	}
 	
 }
