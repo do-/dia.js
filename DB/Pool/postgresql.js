fb14211f9feb33acc86edd7cbb01e92121cf7bf1
Dmitry Ovsyanko <do_@pochta.ru>
2022-08-24 13:58:53 +0300

    pg drop column FIX for missing on_before_drop_column

--- DB/Pool/postgresql.js
+++ DB/Pool/postgresql.js
@@ -774,18 +774,21 @@ module.exports = class extends require ('../Pool.js') {
 
     		if (col === -Infinity) {
     			
-			if (name in existing.columns) {
+				if (name in existing.columns) {
 
-			    let before = table.on_before_drop_column
+					let before = table.on_before_drop_column; if (before) {
 
-			    if (typeof before === 'function') before = before (table)
+						if (typeof before === 'function') before = before (table)
 
-			    let b = before [name]
+						let b = before [name]
 
-			    if (b) for (let i of b) result.push (i)
+						if (b) for (let i of b) result.push (i)
 
-			    result.push ({sql: `ALTER TABLE ${table.qname} DROP COLUMN IF EXISTS "${name}" CASCADE`, params: []})
-			}
+					}
+
+					result.push ({sql: `ALTER TABLE ${table.qname} DROP COLUMN IF EXISTS "${name}" CASCADE`, params: []})
+
+				}
 			
     			delete columns [name]
     			
