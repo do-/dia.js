dae3f1fa9103ce4a289b789c3e4f3bb4113eff81
ral77 <ral77@mail.ru>
2022-09-07 09:25:05 +0300

    w2ui filter - add operator 'is not'

--- Content/Handler/HTTP/Ext/w2ui/Filter.js
+++ Content/Handler/HTTP/Ext/w2ui/Filter.js
@@ -5,7 +5,7 @@ module.exports = class {
 
     op (src) {switch (src) {
         case 'is':       return ' = ?'        
-        case 'not is':   return ' <> ?'        
+        case 'is not':   return ' <> ?'        
         case 'less':     return ' <= ?'        
         case 'more':     return ' >= ?'        
         case 'less!':    return ' < ?'
